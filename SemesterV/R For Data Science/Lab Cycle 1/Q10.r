ca5138611dbe4663c4c60da796e0ea54031ac5a4
NidalNaaz <147641097+NidalNaaz@users.noreply.github.com>
2025-09-14 22:58:35 +0530

    Question 10 of Lab Cycle 1

--- SemesterV/R For Data Science/Lab Cycle 1/Q10.r
+++ SemesterV/R For Data Science/Lab Cycle 1/Q10.r
@@ -8,3 +8,12 @@ for (i in 1:n) {
   rev_lst[i] <- lst[n - i + 1]
 }
 cat("Reversed list:", rev_lst, "\n")
+
+
+"
+Input:
+Enter numbers separated by space: 1 2 3 4 5
+
+Output:
+Reversed list: 5 4 3 2 1
+"
