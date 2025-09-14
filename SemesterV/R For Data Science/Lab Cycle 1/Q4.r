b9ff252fd165207e5c559c3b8c19a2155573157f
NidalNaaz <147641097+NidalNaaz@users.noreply.github.com>
2025-09-14 22:48:54 +0530

    Question 4 of Lab Cycle 1

--- SemesterV/R For Data Science/Lab Cycle 1/Q4.r
+++ SemesterV/R For Data Science/Lab Cycle 1/Q4.r
@@ -2,3 +2,13 @@ len <- as.integer(readline("Enter password length: "))
 chars <- c(letters, LETTERS, 0:9, strsplit("!@#$%^&*()", "")[[1]])
 password <- paste(sample(chars, len, replace = TRUE), collapse = "")
 cat("Generated password:", password, "\n")
+
+
+
+"
+Input:
+Enter password length: 8
+
+Output:
+Generated password: aG4@dK2!
+"
